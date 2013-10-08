9bb229a4d7cc8889a6f43b8bc5a21e44d598e121
Nat <nat.wrw@gmail.com>
2013-10-08 14:36:20 +0700

    Refactor for another image processing service url.

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require("express");
 var http = require('http');
 var path = require('path');
 
+// user config
+var base_service_url = "http://ec2-54-251-88-186.ap-southeast-1.compute.amazonaws.com:3000/fb/";
+
 var app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(express.favicon());
@@ -10,7 +13,7 @@ app.use(express.logger('dev'));
 
 app.get('/:fbuid', function(req, resp) { 
   var fbuid = req.params.fbuid || 'nat.wrw'; 
-  request.get("http://ec2-54-251-88-186.ap-southeast-1.compute.amazonaws.com:3000/fb/"+fbuid).pipe(resp); 
+  request.get(base_service_url+fbuid).pipe(resp); 
 });
 
 
